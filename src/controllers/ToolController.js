cd62329eaed242b3f9652c25394b790f40fa8098
Yago Theodoro <freonzx@gmail.com>
2019-10-18 13:06:09 -0300

    Add simple Tool CRUD ops

--- src/controllers/ToolController.js
+++ src/controllers/ToolController.js
@@ -1,10 +1,36 @@
 const Tool = require('../models/Tool')
 
 class ToolController {
+    async index(req, res) {
+        const tools = await Tool.find()
+
+        return res.json(tools)
+    }
+
     async store(req, res) {
         const tool = await Tool.create(req.body)
 
         return res.status(201).json(tool)
+    }
+
+    async show(req, res) {
+        const tool = await Tool.findById(req.params.id)
+
+        return res.json(tool)
+    }
+
+    async update(req, res) {
+        const tool = await Tool.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+
+        return res.json(tool)
+    }
+
+    async destroy(req, res) {
+        await Tool.findByIdAndDelete(req.params.id)
+
+        return res.status(204).send()
     }
 }
 
--- src/routes.js
+++ src/routes.js
@@ -9,5 +9,9 @@ routes.get('/', (req, res) => res.json({ api_status: 'ok' }))
  * Tool endpoints
  */
 routes.post('/tools', controllers.ToolController.store)
+routes.get('/tools', controllers.ToolController.index)
+routes.get('/tools/:id', controllers.ToolController.show)
+routes.put('/tools/:id', controllers.ToolController.update)
+routes.delete('/tools/:id', controllers.ToolController.destroy)
 
 module.exports = routes
