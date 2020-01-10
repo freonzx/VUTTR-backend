f5388f2219948fd6f9deef3b470cdb5216b22289
Yago Theodoro <freonzx@gmail.com>
2020-01-10 17:46:19 -0300

    Add Joi/express-validation and validators

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const validate = require("express-validation");
 const authMiddleware = require("./middlewares/auth");
 
 const routes = express.Router();
 
+/*
+ * Controllers
+ */
 const UserController = require("./controllers/UserController");
 const SessionController = require("./controllers/SessionController");
 const ToolController = require("./controllers/ToolController");
+
+/*
+ * Validators
+ */
+const UserValidator = require("./validators/User");
+const ToolValidator = require("./validators/Tool");
+const SessionValidator = require("./validators/Session");
 
 /*
  * To check if API is online
@@ -15,8 +26,8 @@ routes.get("/", (req, res) => res.json({ api_status: "ok" }));
 /*
  * User creation
  */
-routes.post("/users", UserController.store);
-routes.post("/sessions", SessionController.store);
+routes.post("/users", validate(UserValidator), UserController.store);
+routes.post("/sessions", validate(SessionValidator), SessionController.store);
 
 /*
  * Tool endpoints
@@ -24,11 +35,14 @@ routes.post("/sessions", SessionController.store);
 routes.get("/tools", ToolController.index);
 routes.get("/tools/:id", ToolController.show);
 
-// Authenticated only
+/*
+ * Authenticated only
+ */
+
 routes.use(authMiddleware);
 
-routes.post("/tools", ToolController.store);
-routes.put("/tools/:id", ToolController.update);
+routes.post("/tools", validate(ToolValidator), ToolController.store);
+routes.put("/tools/:id", validate(ToolValidator), ToolController.update);
 routes.delete("/tools/:id", ToolController.destroy);
 
 module.exports = routes;
